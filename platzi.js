9319cc6a7143b5649081f60e483195b75eb8ca09
Azkorb <87208730+Azkorb@users.noreply.github.com>
2021-09-04 23:29:46 -0500

    Update platzi.js

--- platzi.js
+++ platzi.js
@@ -1,6 +1,12 @@
 var vp = document.getElementById("villaPlatzi");
 var ctx = vp.getContext('2d');
 
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGTH: 39
+};
 
 var fondo = {
     URL: "imagenes/tile.png",
@@ -71,13 +77,6 @@ pollo.imagen.addEventListener("load", cargarPollo);
         ctx.drawImage(vaca.imagen, x, y);
         }
     }
-    if(cerdo.cargaOK == true){
-        for(var v=0; v<3; v++){
-        var x = aleatorio(0, 420);
-        var y = aleatorio(0, 420);
-        ctx.drawImage(cerdo.imagen, x, y);
-        }
-    }
     if(pollo.cargaOK == true){
         for(var v=0; v<10; v++){
         var x = aleatorio(0, 420);
@@ -93,4 +92,46 @@ function aleatorio(min, maxi) {
     var resultado;
     resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
     return resultado;
+}
+var xc = 0;
+var yc = 0;
+
+    cerdo.imagen = new Image();
+    cerdo.imagen.src = cerdo.URL;
+    cerdo.imagen.addEventListener("load", function () {
+        cerdo.cargaOK = true;
+        moverCerdo()
+    });
+
+function moverCerdo(params) {
+
+    ctx.drawImage(cerdo.imagen, xc, yc);
+    document.addEventListener("keydown", coordenadasCerdo);
+
+}
+
+function coordenadasCerdo(evento) 
+{
+    var movimiento = 5;
+    switch (evento.keyCode) {
+        case teclas.UP:
+            ctx.drawImage(cerdo.imagen,xc, yc - movimiento);
+            yc = yc - movimiento;
+            break;
+        case teclas.DOWN:
+            ctx.drawImage(cerdo.imagen,xc, yc + movimiento);
+            yc = yc + movimiento;
+            break;
+        case teclas.LEFT:
+            ctx.drawImage(cerdo.imagen,xc - movimiento, yc);
+            xc = xc - movimiento;
+            break
+        case teclas.RIGTH:
+            ctx.drawImage(cerdo.imagen,xc + movimiento, yc);
+            xc = xc + movimiento;
+            break
+        default:
+            //console.log("otra tecla");
+            break;
+    }
 }
